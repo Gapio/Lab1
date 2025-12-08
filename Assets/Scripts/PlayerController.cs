45166b8ab081afd97f597af9f417aefd9ab8dba1
Gapio <gapakkan@gmail.com>
2025-12-08 13:39:15 +0100

    oops tehe

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    InputAction moveAction;
+    InputAction lookAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        moveAction = InputSystem.actions.FindAction("Move");
+        lookAction = InputSystem.actions.FindAction("Look");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        //move the player based on moveValue
+
+        Vector2 lookValue = lookAction.ReadValue<Vector2>();
+        //rotate the player based on lookValue
     }
 }
