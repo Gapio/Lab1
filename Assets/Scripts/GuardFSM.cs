18be535129e14215c142091248dcb682d0c2fc03
Gapio <gapakkan@gmail.com>
2025-12-10 10:41:30 +0100

    chase n return

--- Assets/Scripts/GuardFSM.cs
+++ Assets/Scripts/GuardFSM.cs
@@ -6,6 +6,9 @@ using UnityEngine.AI;
 public class GuardFSM : MonoBehaviour
 {
     NavMeshAgent agent;
+
+    [SerializeField] private Transform player;
+
     [SerializeField] private List<Transform> waypoints = new List<Transform>(4);
     int currentWaypoint = 0;
 
@@ -28,18 +31,44 @@ public class GuardFSM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         switch (state)
         {
             case GuardState.Patrolling:
-                if(agent.remainingDistance < 1f) {
+                if(!agent.pathPending && agent.remainingDistance < 1f) {
                     agent.SetDestination(waypoints[currentWaypoint].position);
                     currentWaypoint = (currentWaypoint + 1) % waypoints.Count;
                 }
                 break;
             case GuardState.Chasing:
+                agent.SetDestination(player.position);
                 break;
             case GuardState.Returning:
+                if (agent.remainingDistance < 1f)
+                {
+                    Debug.Log("Returned to patrol.");
+                    state = GuardState.Patrolling;
+                }
                 break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player detected!");
+            state = GuardState.Chasing;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player lost!");
+            state = GuardState.Returning;
+            agent.SetDestination(waypoints[currentWaypoint].position);
         }
     }
 }
